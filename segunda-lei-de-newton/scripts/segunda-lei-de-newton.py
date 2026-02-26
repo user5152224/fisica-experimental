36a73d1f1842ee53d75d94e4962ec37df36f14bd
user5152224 <harllen.araujo@gmail.com>
2026-02-25 21:41:19 -0300

    run: 25 de February de 2026 às 21:41

---
+++ segunda-lei-de-newton/scripts/segunda-lei-de-newton.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import math
+
+import estatistica as est
+
+T = [
+  [.139, .140, .141, .141, .140, .141, .139, .141, .141, .141],
+  [.099, .100, .100, .100, .100, .100, .100, .104, .104, .104],
+  [.079, .079, .080, .079, .079, .086, .084, .088, .088, .089],
+  [.070, .070, .072, .074, .070, .080, .078, .081, .082, .082],
+] # segundos
+
+d = .975 # centímetros
+
+V = []
+
+for tempos in T:
+  v = []
+  for t in tempos:
+    v.append(d/t)
+
+  V.append(v)
+
+D = [2.2, 4.4, 6.6, 8.8]
+   
+for v in V:
+  med = est.media(v) 
+  ind = V.index(v)
+  print(f'       {ind+1}. {D[ind]} {med:6.2f} {math.sqrt(med)}')
