44b81f046cfb04922edea6f4b4f7618643bc4109
user5152224 <harllen.araujo@gmail.com>
2026-03-19 17:33:44 -0300

    run: 19.03.26 às 17:33

--- pendulo-simples/scripts/estatistica.py
+++ pendulo-simples/scripts/estatistica.py
@@ -20,16 +20,3 @@ def desvpad(tempos):                # Desvio padrão
 def errest(tempos):                 # Erro estatístico.
   return desvpad(tempos)/math.sqrt(len(tempos))
 
-def errtot(tempos, errinst):        # Erro total.
-  return math.sqrt(errest(tempos)**2+errinst**2)
-
-def prpinc(comp, tempos, errinst):  # Propagação de erros.
-  return math.sqrt( (errest(tempos) * comp/media(tempos)**2)**2 + (errinst/media(tempos))**2 )
-  
-
-mddinst = {              
-# 'Instrumento':[medida(cm), incerteza(cm)],
-  'Fotogate':   [0         , 1/1000   ],
-  'Paquímetro': [9.75      , 1/200    ],
-  'Régua':      [10        , 1/20     ],
-}
