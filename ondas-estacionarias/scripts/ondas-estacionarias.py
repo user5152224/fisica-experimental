383d5ce5ba4a204aaf855ac15f16423ee22abfc5
user5152224 <harllen.araujo@gmail.com>
2026-03-28 20:10:47 -0300

    scripts/

---
+++ ondas-estacionarias/scripts/ondas-estacionarias.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import math
+import estatistica as est
+
+L = [ # Lista de comprimentos (em metro) para cada modo.
+      # modos   1     2     3 
+              [.123, .245, .363], # 60g 
+              [.175, .351, .530], # 110g
+              [.211, .424, .635], # 160g
+              [.250, .498, .745], # 210g
+              [.283, .556, .855], # 260g
+]
+
+LA      = [[2*l/(lens.index(l)+1) for l in lens] for lens in L ] # Lista dos lambdas.          
+MED     = [est.media(lambd) for lambd in LA]                     # Lista de médias.
+QUADMED = [med**2 for med in MED]                                # Quadrado das médias para regressão.
+ERREST  = [est.errest(lambd) for lambd in LA]                    # Erros estatísticos.
+g       = 9.78                                                   # Aceleração gratitacional estipulada (m/s^2)
+MASSAS  = [.060, .110, .160, .210, .260]                         # Massas usadas (em Kg).
+T       = [g*m for m in MASSAS]                                  # Trações (módulo da força peso).
+C       = est.regressao(T, QUADMED)                              # Regressao (y=A+Bx), leia-se [A, B].
+
+print(C)
+
